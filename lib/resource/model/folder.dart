b569227d6f729284064de2571bc0fa9e8401a242
kurioku <148947530+kurioku@users.noreply.github.com>
2023-11-22 17:22:42 -0800

    added models

---
+++ lib/resource/model/folder.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'todo.dart';
+
+part 'folder.freezed.dart';
+part 'folder.g.dart';
+
+@freezed
+class Folder with _$Folder {
+  factory Folder({
+    required String id,
+    required String title,
+    @Default([]) List<Todo> todos,
+  }) = _Folder;
+
+  factory Folder.fromJson(Map<String, dynamic> json) => _$FolderFromJson(json);
+}
