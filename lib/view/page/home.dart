09f41d46f17d95f211968d52125b22cc0acee729
kurioku <148947530+kurioku@users.noreply.github.com>
2023-11-22 23:47:50 -0800

    expansion tile

--- lib/view/page/home.dart
+++ lib/view/page/home.dart
@@ -11,16 +11,13 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final watch = ref.watch(todosPod);
     final read = ref.read(todosPod.notifier);
+    final completed = watch.where((t) => t.check).toList();
+    final active = watch.where((t) => !t.check).toList();
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: const Text('Folders'),
+        title: const Text('Todo'),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              context.go('/settings');
-            },
-          ),
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
@@ -28,7 +25,7 @@ class HomePage extends ConsumerWidget {
                 context: context,
                 builder: (context) {
                   return AlertDialog(
-                    title: const Text('Add Folder'),
+                    title: const Text('Add Todo'),
                     content: TextField(
                       autofocus: true,
                       onSubmitted: (title) {
@@ -43,23 +40,45 @@ class HomePage extends ConsumerWidget {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: watch.length,
-        itemBuilder: (context, index) {
-          return Dismissible(
-            key: ValueKey(watch[index].id),
-            onDismissed: (_) {
-              read.remove(watch[index]);
+      body: Column(
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: active.length,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: ValueKey(active[index].id),
+                onDismissed: (_) => read.remove(active[index]),
+                child: CheckboxListTile(
+                  title: Text(active[index].title),
+                  value: active[index].check,
+                  onChanged: (_) => read.check(active[index].id),
+                ),
+              );
             },
-            child: CheckboxListTile(
-              title: Text(watch[index].title),
-              value: watch[index].check,
-              onChanged: (_) {
-                read.check(watch[index].id);
-              },
-            ),
-          );
-        },
+          ),
+          const Spacer(),
+          ExpansionTile(
+            title: Text('Completed ${completed.length}'),
+            children: [
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: completed.length,
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    key: ValueKey(completed[index].id),
+                    onDismissed: (_) => read.remove(completed[index]),
+                    child: CheckboxListTile(
+                      title: Text(completed[index].title),
+                      value: completed[index].check,
+                      onChanged: (_) => read.check(completed[index].id),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
