54c3ef9f9f7b0acbcf1f72b8b07cf6e9dc8089a4
kurioku <148947530+kurioku@users.noreply.github.com>
2023-11-22 23:51:46 -0800

    restructure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'core/router/router.dart';
+import 'view/router/router.dart';
 
 void main() {
   runApp(const ProviderScope(child: MainApp()));
