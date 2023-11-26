213259314b041fd1fc5863ce060feb41ed6c5edf
kurioku <148947530+kurioku@users.noreply.github.com>
2023-11-25 22:17:03 -0800

    themes

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,7 +1,67 @@
 import 'package:flutter/material.dart';
 
 class Themes {
-  static final light = ThemeData.light().copyWith();
+  static final light = ThemeData.light().copyWith(
+    appBarTheme: const AppBarTheme(
+      shape: Border(
+        bottom: BorderSide(color: Colors.black),
+      ),
+    ),
+    colorScheme: const ColorScheme.light(
+      primary: Colors.blue,
+      onPrimary: Colors.black,
+      surfaceTint: Colors.transparent,
+    ),
+    dividerTheme: const DividerThemeData(color: Colors.black26),
+    dialogTheme: const DialogTheme(
+      titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        side: BorderSide(color: Colors.black),
+      ),
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.zero),
+      ),
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(fontSize: 20, color: Colors.black),
+    ),
+  );
 
-  static final dark = ThemeData.dark().copyWith();
+  static final dark = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      shape: Border(
+        bottom: BorderSide(color: Colors.white54),
+      ),
+    ),
+    colorScheme: const ColorScheme.dark(
+      primary: Colors.blue,
+      onPrimary: Colors.white,
+      surface: Colors.black,
+      surfaceTint: Colors.transparent,
+    ),
+    dividerTheme: const DividerThemeData(color: Colors.white24),
+    dialogTheme: const DialogTheme(
+      backgroundColor: Colors.black,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        side: BorderSide(color: Colors.white),
+      ),
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.zero),
+      ),
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(fontSize: 20),
+    ),
+  );
 }
