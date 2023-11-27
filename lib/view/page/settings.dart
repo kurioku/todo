4c84c797715b054aa322796f885cd92ed45bda7d
kurioku <148947530+kurioku@users.noreply.github.com>
2023-11-26 18:17:30 -0800

    updated

--- lib/view/page/settings.dart
+++ lib/view/page/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../provider/git_pod.dart';
 import '/core/theme/theme_pod.dart';
 
 class Settings extends ConsumerWidget {
@@ -12,25 +13,53 @@ class Settings extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: ListTile(
-        title: const Text('Theme Mode'),
-        trailing: DropdownButton(
-          value: ref.watch(themeSPod),
-          onChanged: (v) => ref.read(themeSPod.notifier).change(v!),
-          items: const [
-            DropdownMenuItem(
-              value: 'system',
-              child: Text('System'),
-            ),
-            DropdownMenuItem(
-              value: 'light',
-              child: Text('Light'),
-            ),
-            DropdownMenuItem(
-              value: 'dark',
-              child: Text('Dark'),
-            ),
-          ],
+      body: Container(
+        padding: const EdgeInsets.all(18),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Theme.of(context).hintColor),
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const ListTile(
+                leading: Icon(Icons.info_outline),
+                title: Text('Version'),
+                subtitle: Text('0.1.0'),
+              ),
+              ListTile(
+                  leading: const Icon(Icons.code),
+                  title: const Text('Source Code'),
+                  onTap: () => ref.read(gitPod)),
+              const ListTile(
+                leading: Icon(Icons.person_outline),
+                title: Text('Developed by kurioku'),
+              ),
+              ListTile(
+                title: const Text('Theme Mode'),
+                leading: ref.watch(themeIconPod),
+                trailing: DropdownButton(
+                  value: ref.watch(themeIntPod),
+                  onChanged: (v) => ref.read(themeIntPod.notifier).change(v!),
+                  items: const [
+                    DropdownMenuItem(
+                      value: 0,
+                      child: Text('System'),
+                    ),
+                    DropdownMenuItem(
+                      value: 1,
+                      child: Text('Light'),
+                    ),
+                    DropdownMenuItem(
+                      value: 2,
+                      child: Text('Dark'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
